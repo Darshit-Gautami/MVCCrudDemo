35766ef71832fb15887c7078120d7a4bc0796868
darshit <darshit.gautami@coruscatesolutions.in>
2020-06-02 13:45:41 +0530

    First Commit

---
+++ CRUD_jQuery_MVC/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_jQuery_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            demoEntities entities = new demoEntities();
+            List<Customer> customers = entities.Customers.ToList();
+
+            //Add a Dummy Row.
+            customers.Insert(0, new Customer());
+            return View(customers);
+        }
+
+        [HttpPost]
+        public JsonResult InsertCustomer(Customer customer)
+        {
+            using (demoEntities entities = new demoEntities())
+            {
+                entities.Customers.Add(customer);
+                entities.SaveChanges();
+            }
+
+            return Json(customer);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            using (demoEntities entities = new demoEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customers
+                                            where c.CustomerId == customer.CustomerId
+                                            select c).FirstOrDefault();
+                updatedCustomer.Name = customer.Name;
+                updatedCustomer.Country = customer.Country;
+                entities.SaveChanges();
+            }
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomer(int customerId)
+        {
+            using (demoEntities entities = new demoEntities())
+            {
+                Customer customer = (from c in entities.Customers
+                                     where c.CustomerId == customerId
+                                     select c).FirstOrDefault();
+                entities.Customers.Remove(customer);
+                entities.SaveChanges();
+            }
+            return new EmptyResult();
+        }
+    }
+}
